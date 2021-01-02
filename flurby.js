438a7ac88cad29ec8e3c5e767fe80fa4c9ccb2fd
Ross Payne <rosspayn3@gmail.com>
2021-01-01 18:03:30 -0600

    updated formatting

--- flurby.js
+++ flurby.js
@@ -83,7 +83,7 @@ client.on('guildMemberAdd', member => {
     // Do nothing if the channel wasn't found on this server
     if (!channel) return;
     // Send the message, mentioning the member
-    channel.send(`Welcome to the server, ${member}`);
+    channel.send(`Welcome to the server, ${member}!`);
 });
 
 
